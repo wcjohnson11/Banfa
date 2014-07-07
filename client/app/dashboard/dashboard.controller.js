5897745baf83250c803513b01c5d4f635f571d0e
wcjohnson11 <williamcouperjohnson@gmail.com>
2014-07-07 16:43:32 -0700

    add basic challenge function

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -6,4 +6,17 @@ angular.module('banfaApp')
     $http.get('/api/users').success(function(users) {
       $scope.users = users;
     });
+    $scope.challenge = function(user) {
+	  console.log('challenge', user);
+    };
+/*  Set up to be challenge instead of delete
+    $scope.delete = function(user) {
+      User.remove({ id: user._id });
+      angular.forEach($scope.users, function(u, i) {
+        if (u === user) {
+          $scope.users.splice(i, 1);
+        }
+      });
+    };
+    */
   });
