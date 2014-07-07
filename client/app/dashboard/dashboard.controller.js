239502986e7a797ac01348b575676f7c33f12364
wcjohnson11 <williamcouperjohnson@gmail.com>
2014-07-07 16:34:19 -0700

    implement user list in dashboard

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -1,6 +1,9 @@
 'use strict';
 
 angular.module('banfaApp')
-  .controller('DashboardCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('DashboardCtrl', function ($scope, $http) {
+
+    $http.get('/api/users').success(function(users) {
+      $scope.users = users;
+    });
   });
--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -4,7 +4,7 @@ angular.module('banfaApp')
   .controller('NavbarCtrl', function ($scope, $location, Auth) {
     $scope.menu = [{
       'title': 'Home',
-      'link': '/'
+      'link': '/dashboard'
     }];
 
     $scope.isCollapsed = true;
